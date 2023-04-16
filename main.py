c2735d7719932423e55916c845edae5c49c95b63
yuuliyz <130220904+yuuliyz@users.noreply.github.com>
2023-04-16 12:45:59 +0500

    v 0.0.3
    Дописан класс GameSprite

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from pygame import *
 init()
 
 class GameSprite(sprite.Sprite):
-    pass
+    def init(self, sprite_image, sprite_x, sprite_y, sprite_width, sprite_height, sprite_speed):
+        super().__init__()
+        self.speed = sprite_speed
+        self.width = sprite_width
+        self.height = sprite_height
+        self.image = transform.scale(image.load(sprite_image), (self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rect.x = sprite_x
+        self.rect.y = sprite_y
+    def reset(self):
+        scene.blit(self.image, (self.rect.x, self.rect.y)) 
 
 class Player(GameSprite):
     pass
