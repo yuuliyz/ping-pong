f738f442f38a5fdcb5dbeb79e5de11044f74871e
yuuliyz <130220904+yuuliyz@users.noreply.github.com>
2023-04-22 16:36:35 +0500

    v 0.0.5

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from pygame import *
 init()
 
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption('Пинг-понг')
+background = transform.scale(image.load('image.png'), (win_width, win_height))
+
+FPS = 60
+game = True
+update = True
+
 class GameSprite(sprite.Sprite):
     def init(self, sprite_image, sprite_x, sprite_y, sprite_width, sprite_height, sprite_speed):
         super().__init__()
@@ -24,3 +34,18 @@ class Player(GameSprite):
 
 class Ball(GameSprite):
     pass
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game == False
+    
+    
+    if update:
+        window.blit(background, (0, 0))
+        
+        
+        
+display.update()
+clock.tick(FPS)
+        
