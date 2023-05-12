50d443b26adc6e4189d0e5a28b9b073da3435680
yuuliyz <130220904+yuuliyz@users.noreply.github.com>
2023-05-12 21:07:07 +0500

    v 0.1.2
    исправляю ошибки

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ FPS = 60
 game = True
 update = True
 
+clock = time.Clock()
 score1 = 0
 score2 = 0
 
+start_x = win_width / 2
+start_y = win_height / 2
+
 class GameSprite(sprite.Sprite):
-    def init(self, sprite_image, sprite_x, sprite_y, sprite_width, sprite_height, sprite_speed):
+    def __init__(self, sprite_image, sprite_x, sprite_y, sprite_width, sprite_height, sprite_speed):
         super().__init__()
-        self.speed = sprite_speed
+        self.speed_x = sprite_speed
+        self.speed_y = sprite_speed
         self.width = sprite_width
         self.height = sprite_height
         self.image = transform.scale(image.load(sprite_image), (self.width, self.height))
@@ -28,32 +33,42 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y)) 
 
-class Player(GameSprite):
+class Player1(GameSprite):
     def update(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 0:
-            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win.height - self.height:
-            self.rect.y += self.speed
+            self.rect.y -= self.speed_y
+        if keys[K_DOWN] and self.rect.y < win_height - self.height:
+            self.rect.y += self.speed_y
+
+class Player2(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed_y
+        if keys[K_s] and self.rect.y < win_height - self.height:
+            self.rect.y += self.speed_y
+
 
 class Ball(GameSprite):
     def update(self):
-        self.rect.x += self.speed
-        self.rect.y += self.speed
-        if self.rect.x < 0:
-            score2 += 1
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.y > win_height - self.height:
+            self.speed_y *= -1
             
-        if self.rect.x > win_width - self.width:
-            score1 += 1
+        if self.rect.y < 0:
+            self.speed_y *= -1
 
     def move(self):
-        self.speed * -1
+        self.speed_x *= -1
+
 
     
 
-platform_1 = Player('image2.png', 0, 0, 30, 100, 10)
-platform_2 = Player('image2.png', 670, 0, 30, 100, 10)
-ball = Ball('image4.png', 350, 250, 50, 50, 3)
+platform_1 = Player1('image2.jpg', 0, 0, 30, 100, 10)
+platform_2 = Player2('image2.jpg', 670, 0, 30, 100, 10)
+ball = Ball('image4.jpg', 350, 250, 50, 50, 3)
 
 
 
@@ -74,6 +89,10 @@ while game:
 
     if sprite.collide_rect(platform_1, ball) or sprite.collide_rect(platform_2, ball):
         ball.move()
+    if ball.rect.x < 0:
+        score2 += 1
+        ball.rect.x = start_x
+        ball.rect.y = start_y
 
     if score1 > 5:
         update = False
@@ -87,4 +106,3 @@ while game:
         
     display.update()
     clock.tick(FPS)
-        
